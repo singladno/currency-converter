00d9431c6fb054901cf36aa2364a9ddd47abbfff
Саттаров Дали Саттарович <singladno@gmail.com>
2022-04-07 12:22:26 +0800

    hide view switcher when no data loaded

--- src/components/Main/ViewSwitcher/index.tsx
+++ src/components/Main/ViewSwitcher/index.tsx
@@ -4,28 +4,31 @@ import { ViewType } from "@/reducers/view";
 import { useAppTranslate } from "@/common/hooks/useTranslate";
 import { setView } from "@/reducers/view";
 import { useAppDispatch } from "@/actions";
-import { viewSelector } from "@/selectors";
+import { coursesSelector, viewSelector } from "@/selectors";
 
 export const ViewSwitcher: React.FC = () => {
   const { t } = useAppTranslate();
   const dispatch = useAppDispatch();
+  const courses = coursesSelector();
   const view = viewSelector();
   return (
-    <Nav
-      justify
-      variant="tabs"
-      className="w-75 align-self-center"
-      onSelect={(v) => dispatch(setView(v as ViewType))}
-      activeKey={view}
-    >
-      {Object.keys(ViewType).map((key) => {
-        const eventKey = ViewType[key];
-        return (
-          <Nav.Item key={eventKey}>
-            <Nav.Link eventKey={eventKey}>{t(eventKey)}</Nav.Link>
-          </Nav.Item>
-        );
-      })}
-    </Nav>
+    (!!courses.length && (
+      <Nav
+        justify
+        variant="tabs"
+        className="w-75 align-self-center"
+        onSelect={(v) => dispatch(setView(v as ViewType))}
+        activeKey={view}
+      >
+        {Object.keys(ViewType).map((key) => {
+          const eventKey = ViewType[key];
+          return (
+            <Nav.Item key={eventKey}>
+              <Nav.Link eventKey={eventKey}>{t(eventKey)}</Nav.Link>
+            </Nav.Item>
+          );
+        })}
+      </Nav>
+    )) || <></>
   );
 };
